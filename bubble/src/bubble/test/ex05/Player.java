a71cbad3ce0f202fd3b1ff4e69319bf5d75f5d14
wjdtndk <jungswa1204@gmail.com>
2024-02-20 20:11:56 +0900

    Add character jump functionality

---
+++ bubble/src/bubble/test/ex05/Player.java
@@ -0,0 +1,194 @@
+package bubble.test.ex05;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Player extends JLabel implements Movable {
+
+	// 위치 상태
+	private int x;
+	private int y;
+
+	// 움직임 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+
+	// 플레이어 속도 상태
+	private final int SPEED = 4;
+	private final int JUMPSPEED = 2; // up, down
+
+	private ImageIcon playerR, playerL;
+
+	public Player() {
+		initObject();
+		initSetting();
+	}
+
+	private void initObject() {
+		playerR = new ImageIcon("image/playerR.png");
+		playerL = new ImageIcon("image/playerL.png");
+	}
+
+	private void initSetting() {
+		x = 55;
+		y = 535;
+
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+
+		setIcon(playerR);
+		setSize(50, 50);
+		setLocation(x, y);
+	}
+
+	// 이벤트 핸들러
+	@Override
+	public void left() {
+		System.out.println("left");
+		left = true;
+		new Thread(() -> {
+			while (left) {
+				setIcon(playerL);
+				x = x - SPEED;
+				setLocation(x, y);
+				try {
+					Thread.sleep(10); // 0.01초
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+
+	}
+
+	@Override
+	public void right() {
+		System.out.println("right");
+		right = true;
+		new Thread(() -> {
+			while (right) {
+				setIcon(playerR);
+				x = x + SPEED;
+				setLocation(x, y);
+				try {
+					Thread.sleep(10); // 0.01초
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+
+	}
+
+	// left + up, right + up 동시에 실행하려면
+	// 스레드 만들어야 가능!
+	@Override
+	public void up() {
+		System.out.println("up");
+		up = true;
+		new Thread(() -> {
+			for (int i = 0; i < 130 / JUMPSPEED; i++) {
+				y = y - JUMPSPEED;
+				setLocation(x, y);
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			up = false;
+			down();
+			
+		}).start();
+	}
+
+	@Override
+	public void down() {
+		System.out.println("down");
+		down = true;
+		new Thread(()->{
+			for (int i = 0; i < 130 / JUMPSPEED; i++) {
+				y = y + JUMPSPEED;
+				setLocation(x, y);
+				try {
+					Thread.sleep(3);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			down = false; 
+		}).start();
+		
+	}
+
+	// Getter/Setter
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
+	public ImageIcon getPlayerR() {
+		return playerR;
+	}
+
+	public void setPlayerR(ImageIcon playerR) {
+		this.playerR = playerR;
+	}
+
+	public ImageIcon getPlayerL() {
+		return playerL;
+	}
+
+	public void setPlayerL(ImageIcon playerL) {
+		this.playerL = playerL;
+	}
+
+}
